7075cba292dbb1bf2d2c0553c48238b5f07d8aee
tribbianiwang <1130455004@qq.com>
2019-09-20 17:08:25 +0800

    协程hello，world

--- app/src/main/java/com/wl/androiddemos/MainActivity.kt
+++ app/src/main/java/com/wl/androiddemos/MainActivity.kt
@@ -2,11 +2,29 @@ package com.wl.androiddemos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
+    val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        testGlobalScope()
+    }
+
+
+    fun testGlobalScope(){
+        GlobalScope.launch { // 在后台启动一个新的协程并继续
+            delay(1000L) // 非阻塞的等待 1 秒钟（默认时间单位是毫秒）
+
+            Log.d(TAG,"World!") // 在延迟后打印输出
+        }
+        Log.d(TAG,"Hello,") // 协程已在等待时主线程还在继续
+        Thread.sleep(2000L) // 阻塞主线程 2 秒钟来保证 JVM 存活
     }
 }
