77ff12c64840bf47a9ad6d162bbfa0f00bb437d0
tribbianiwang <1130455004@qq.com>
2019-10-09 17:13:31 +0800

    协程嵌套第一种写法

--- app/src/main/java/com/wl/androiddemos/MainActivity.kt
+++ app/src/main/java/com/wl/androiddemos/MainActivity.kt
@@ -4,10 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
 import android.util.Log
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,14 +19,25 @@ class MainActivity : AppCompatActivity() {
 
 
     fun testGlobalScope(){
-        GlobalScope.launch { // 在后台启动一个新的协程并继续
-            delay(1000L) // 非阻塞的等待 1 秒钟（默认时间单位是毫秒）
-
-            Log.d(TAG,"World!") // 在延迟后打印输出
+//        GlobalScope.launch { // 在后台启动一个新的协程并继续
+//            delay(1000L) // 非阻塞的等待 1 秒钟（默认时间单位是毫秒）
+//
+//            Log.d(TAG,"World!") // 在延迟后打印输出
+//        }
+//        Log.d(TAG,"Hello,") // 协程已在等待时主线程还在继续
+//       runBlocking {// 但是这个表达式阻塞了主线程
+//           delay(2000L)// ……我们延迟 2 秒来保证 JVM 的存活
+//       }
+      val  coroutinContext: CoroutineContext = EmptyCoroutineContext
+        val coroutineScope = CoroutineScope(coroutinContext)
+        coroutineScope.launch(Dispatchers.IO) {
+            Log.d(TAG,"threadname:io"+Thread.currentThread().name)
+            coroutineScope.launch (Dispatchers.Main){
+                Log.d(TAG,"threadname:main"+Thread.currentThread().name)
+            }
         }
-        Log.d(TAG,"Hello,") // 协程已在等待时主线程还在继续
-       runBlocking {// 但是这个表达式阻塞了主线程
-           delay(2000L)// ……我们延迟 2 秒来保证 JVM 的存活
-       }
+
     }
+
+
 }
