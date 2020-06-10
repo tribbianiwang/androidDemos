13807e096ef674f0dca2ce1bb34a6ec94431ef21
tribbianiwang <1130455004@qq.com>
2020-06-10 14:01:47 +0800

    ConstraintLayout学习1

---
+++ app/src/main/java/com/wl/androiddemos/activity/KotlinTestActivity.kt
@@ -0,0 +1,14 @@
+package com.wl.androiddemos.activity
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.wl.androiddemos.R
+
+class KotlinTestActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_kotlin_test)
+    }
+}
