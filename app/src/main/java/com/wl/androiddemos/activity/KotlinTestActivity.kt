185f69419d8f70b083f654b772d1579756143138
tribbianiwang <1130455004@qq.com>
2021-03-04 13:20:22 +0800

    Chain style

--- app/src/main/java/com/wl/androiddemos/activity/KotlinTestActivity.kt
+++ app/src/main/java/com/wl/androiddemos/activity/KotlinTestActivity.kt
@@ -2,6 +2,8 @@ package com.wl.androiddemos.activity
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.wl.androiddemos.R
 
@@ -10,5 +12,15 @@ class KotlinTestActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlin_test)
+        var age = -12
+        require(age>0){
+            "age must > 0"
+        }
+
+        var name = ""
+        checkNotNull(name){
+         "name is not null"
+        }
+
     }
 }
